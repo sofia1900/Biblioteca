8177323efec3022f294112ec6deb1d3b63e85858
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2022-10-26 19:13:03 +0200

    Proyecto Biblioteca

--- src/com/sofiajimlap/biblioteca/Author.java
+++ src/com/sofiajimlap/biblioteca/Author.java
@@ -14,4 +14,19 @@ public class Author {
 
     //Metodos
 
+        public Integer getId() {return id;}
+        public void setId(Integer id) {this.id = id;}
+
+        public String getName() {return name;}
+        public void setName(String name){this.name = name;}
+
+        public String getSurname() {return surname;}
+        public void setSurname(String surname) {this.surname = surname;}
+
+        public Date getBirthday() {return birthday;}
+        public void setBirthday(Date birthday) {this.birthday = birthday;}
+
+        public String getPlaceBrith() {return placeBrith;}
+        public void setPlaceBrith(String placeBrith) {this.placeBrith = placeBrith;}
+
 }
