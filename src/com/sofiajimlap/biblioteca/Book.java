53d8b99368f7417460ff8488dcd3cf8fa01d7f0c
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2022-10-26 19:36:16 +0200

    Proyecto Biblioteca

--- src/com/sofiajimlap/biblioteca/Book.java
+++ src/com/sofiajimlap/biblioteca/Book.java
@@ -1,31 +1,40 @@
 package com.sofiajimlap.biblioteca;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Book {
 
     //Atributos
 
-    private Integer id;
-    private String title;
-    private String isbn;
-    private Date publicationDate;
+        private Integer id;
+        private String title;
+        private String isbn;
+        private Date publicationDate;
         //autores
+        private ArrayList<Author> authors = new ArrayList<>();
         //generos
+        private ArrayList<Genre> genres = new ArrayList<>();
 
 
     //Metodos
 
-    public Integer getId(){return id;}
-    public void setId (Integer id){this.id = id;}
+        public Integer getId(){return id;}
+        public void setId (Integer id){this.id = id;}
 
-    public String getTitle(){return title;}
-    public void setTitle (String title){this.title = title;}
+        public String getTitle(){return title;}
+        public void setTitle (String title){this.title = title;}
 
-    public String getIsbn (){return isbn;}
-    public void setIsbn (String isbn){this.isbn = isbn;}
+        public String getIsbn (){return isbn;}
+        public void setIsbn (String isbn){this.isbn = isbn;}
 
-    public Date getPublicationDate() {return publicationDate;}
-    public void setPublicationDate (Date publicationDate){this.publicationDate = publicationDate;}
+        public Date getPublicationDate() {return publicationDate;}
+        public void setPublicationDate (Date publicationDate){this.publicationDate = publicationDate;}
+
+        public void addAuthor (Author author) {this.authors.add(author);}
+        public ArrayList<Author> setAuthors() {return this.authors;}
+
+        public void addGenre (Genre genre) {this.genres.add(genre);}
+        public ArrayList<Genre> setGenre() {return this.genres;}
 
 }
--- src/com/sofiajimlap/biblioteca/Reserve.java
+++ src/com/sofiajimlap/biblioteca/Reserve.java
@@ -1,28 +1,36 @@
 package com.sofiajimlap.biblioteca;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Reserve {
 
     //Atributos
 
-    private Integer id;
-    private Date startDate;
-    private Date stopDate;
-
-    //USER
-    //BOOK
+        private Integer id;
+        private Date startDate;
+        private Date stopDate;
+        //USER
+        private User user;
+        //BOOK
+        private Book book;
 
 
     //Metodos
 
-    public Integer getId(){return id;}
-    public void setId (Integer id){this.id = id;}
+        public Integer getId(){return id;}
+        public void setId (Integer id){this.id = id;}
 
-    public Date getStartDate(){return startDate;}
-    public void setStartDate(Date startDate){this.startDate = startDate;}
+        public Date getStartDate(){return startDate;}
+        public void setStartDate(Date startDate){this.startDate = startDate;}
 
-    public Date getStopDate(){return stopDate;}
-    public void setStopDate(Date startDate){this.stopDate = stopDate;}
+        public Date getStopDate(){return stopDate;}
+        public void setStopDate(Date startDate){this.stopDate = stopDate;}
+
+        public User getUser() {return user;}
+        public void setUser(User user) {this.user = user;}
+
+        public Book getBook() {return book;}
+        public void serBook(Book book) {this.book = book;}
 
 }
